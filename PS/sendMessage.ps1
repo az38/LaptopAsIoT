97131c5902593008d3db2a37cbdfe618348f3bb8
Aleksei Zhukov <59407868+az38@users.noreply.github.com>
2023-11-21 21:24:18 +0100

    Create sendMessage.ps1

---
+++ PS/sendMessage.ps1
@@ -0,0 +1,21 @@
+$storageAccount = $env:IOT_StorageAccount
+$container = $env:IOT_Container
+$SASToken = $env:IOT_SASToken_Blob
+
+$dateFolder = (Get-Date).ToString("yyyy/MM/dd")
+$fileTimestamp = (Get-Date(Get-Date).ToUniversalTime() -UFormat %s) + ".json"
+$fileTimestampName = "$PSScriptRoot\$fileTimestamp"
+$blob = "$dateFolder/$fileTimestamp"
+
+$Context = New-AzStorageContext -StorageAccountName $storageAccount -SasToken $SASToken 
+
+$data = @{}
+$data["LogicalDisks"] = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object DeviceID, Size, FreeSpace
+$data["Memory"] = Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_Memory | Select-Object AvailableKBytes
+$data["UTC"] = Get-CimInstance -ClassName Win32_UTCTime
+$data | ConvertTo-JSON | Out-File -FilePath "$fileTimestampName"
+
+
+Set-AzStorageBlobContent -Container $container -File $fileTimestampName -Blob $blob -Context $Context -Force
+
+Remove-Item -Path $fileTimestampName -Force
